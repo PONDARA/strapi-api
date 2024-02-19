df3e5a1b69a35840a036bffc73fb9c3d4b2ed566
Pun Dara <pun.dara@DEPDARA.sabay.office>
2024-02-19 16:58:32 +0700

    init strapi project with restaurant and category

---
+++ src/api/restautant/controllers/restautant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restautant controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restautant.restautant');
---
+++ src/api/restautant/routes/restautant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restautant router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::restautant.restautant');
